9133241205ee827dbbabb2a35724aad2f2db4646
j-bogart <24586340+j-bogart@users.noreply.github.com>
2023-09-10 09:58:35 +0200

    feat: create request controller

---
+++ src/main/java/fr/jbogart/fizzbuzz/FizzbuzzController.java
@@ -0,0 +1,37 @@
+package fr.jbogart.fizzbuzz;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/fizzbuzz")
+public class FizzbuzzController {
+
+    private final FizzbuzzService fizzbuzzService;
+
+    public FizzbuzzController(FizzbuzzService fizzbuzzService) {
+        this.fizzbuzzService = fizzbuzzService;
+    }
+
+    @PostMapping(consumes = {"application/json"})
+    public ResponseEntity<List<String>> generateFizzbuzz(@RequestBody FizzbuzzRequest body) {
+
+        if (body.getInt1() < 1 || body.getInt2() < 1 || body.getLimit() < 1) {
+            return ResponseEntity.status(HttpStatus.UNPROCESSABLE_ENTITY).build();
+        }
+
+        if (body.getStr1() == null || body.getStr2() == null) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        List<String> strings = fizzbuzzService.generateSequence(body.getInt1(), body.getInt2(), body.getLimit(), body.getStr1(), body.getStr2());
+        return ResponseEntity.status(HttpStatus.CREATED).body(strings);
+    }
+
+}
---
+++ src/main/java/fr/jbogart/fizzbuzz/FizzbuzzRequest.java
@@ -0,0 +1,32 @@
+package fr.jbogart.fizzbuzz;
+
+public class FizzbuzzRequest {
+
+
+    private int int1;
+    private int int2;
+    private int limit;
+    private String str1;
+    private String str2;
+
+    public Integer getInt1() {
+        return int1;
+    }
+
+    public Integer getInt2() {
+        return int2;
+    }
+
+    public Integer getLimit() {
+        return limit;
+    }
+
+    public String getStr1() {
+        return str1;
+    }
+
+    public String getStr2() {
+        return str2;
+    }
+
+}
